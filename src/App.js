8ce94236e5f18b08784870f73443fbdb5281d404
pmccart6 <52436268+pmccart6@users.noreply.github.com>
2024-06-17 16:57:17 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import React, {useEffect, useState} from 'react';
+import React from 'react';
 import Educ from './Educ';
 import Exper from './Exper';
 import LandPage from './landPage';
